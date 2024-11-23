a388ed152c7af3c8f89120692a5a13a53a7a7e53
윤동현 <82762769+donghyun81@users.noreply.github.com>
2024-11-23 19:45:03 +0900

    refactor(Month): 일 리스트를 간략하게 수정

--- src/main/kotlin/oncall/Month.kt
+++ src/main/kotlin/oncall/Month.kt
@@ -1,34 +1,34 @@
 package oncall
 
-enum class Month(val month:Int,val days:List<Int>) {
-    JANUARY(1,listOf(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31)),
-    FEBRUARY(2,listOf(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28)),
-    MARCH(3,listOf(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31)),
-    APRIL(4,listOf(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30)),
-    MAY(5,listOf(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31)),
-    JUNE(6,listOf(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30)),
-    JULY(7,listOf(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31)),
-    AUGUST(8,listOf(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31)),
-    SEPTEMBER(9,listOf(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30)),
-    OCTOBER(10,listOf(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31)),
-    NOVEMBER(11,listOf(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30)),
-    DECEMBER(12,listOf(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31));
+enum class Month(val month: Int, val days: List<Int>) {
+    JANUARY(1, (1..31).toList()),
+    FEBRUARY(2, (1..28).toList()),
+    MARCH(3, (1..31).toList()),
+    APRIL(4, (1..30).toList()),
+    MAY(5, (1..31).toList()),
+    JUNE(6, (1..30).toList()),
+    JULY(7, (1..31).toList()),
+    AUGUST(8, (1..31).toList()),
+    SEPTEMBER(9, (1..30).toList()),
+    OCTOBER(10, (1..31).toList()),
+    NOVEMBER(11, (1..30).toList()),
+    DECEMBER(12, (1..31).toList());
 
-    companion object{
-        fun convertMonth(monthNumber: Int):Month{
-         when(monthNumber){
-             1 -> return JANUARY
-             2 -> return FEBRUARY
-             3 -> return MARCH
-             4 -> return APRIL
-             5 -> return MAY
-             6 -> return JUNE
-             7 -> return JULY
-             8 -> return AUGUST
-             9 -> return SEPTEMBER
-             10 -> return OCTOBER
-             11 ->return  NOVEMBER
-         }
+    companion object {
+        fun convertMonth(monthNumber: Int): Month {
+            when (monthNumber) {
+                1 -> return JANUARY
+                2 -> return FEBRUARY
+                3 -> return MARCH
+                4 -> return APRIL
+                5 -> return MAY
+                6 -> return JUNE
+                7 -> return JULY
+                8 -> return AUGUST
+                9 -> return SEPTEMBER
+                10 -> return OCTOBER
+                11 -> return NOVEMBER
+            }
             return DECEMBER
         }
     }
