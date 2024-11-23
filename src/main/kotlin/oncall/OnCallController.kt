a58cc88ec997e0b378e956a4fdaa08b78d43e94c
윤동현 <82762769+donghyun81@users.noreply.github.com>
2024-11-23 18:19:20 +0900

    feat(OnCallController): 비상 근무 월과 시작 요일을 반환하는 기능 추가

---
+++ src/main/kotlin/oncall/OnCallController.kt
@@ -0,0 +1,20 @@
+package oncall
+
+class OnCallController {
+    private val inputView = InputView()
+
+    fun run() {
+        val (month, startDayOfWeek) = getEmergencyWorkDays()
+    }
+
+    private fun getEmergencyWorkDays(): Pair<Int, String> {
+        val emergencyWorkDaysInput = inputView.readEmergencyWorkDays().split(",")
+        require(emergencyWorkDaysInput.size == 2) { "[ERROR] 유효하지 않은 입력 값입니다. 다시 입력해 주세요." }
+        val (emergencyMonthInput, startDayOfWeekInput) = emergencyWorkDaysInput
+        val emergencyMonth =
+            requireNotNull(emergencyMonthInput.toIntOrNull()) { "[ERROR] 유효하지 않은 입력 값입니다. 다시 입력해 주세요." }
+        require(emergencyMonth in 1..12) { "[ERROR] 유효하지 않은 입력 값입니다. 다시 입력해 주세요." }
+        require(startDayOfWeekInput in WEEK.entries.map { it.text }) { "[ERROR] 유효하지 않은 입력 값입니다. 다시 입력해 주세요." }
+        return Pair(emergencyMonth, startDayOfWeekInput)
+    }
+}
