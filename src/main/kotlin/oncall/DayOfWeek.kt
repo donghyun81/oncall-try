82e62a23f95a231456b912347c748b00d7ac6da3
윤동현 <82762769+donghyun81@users.noreply.github.com>
2024-11-23 19:03:05 +0900

    feat(OnCallController): 요일 객체를 반환하도록 변경

---
+++ src/main/kotlin/oncall/DayOfWeek.kt
@@ -0,0 +1,25 @@
+package oncall
+
+enum class DayOfWeek(val text: String) {
+    MONDAY("월"),
+    TUESDAY("화"),
+    WEDNESDAY("수"),
+    THURSDAY("목"),
+    FRIDAY("금"),
+    SATURDAY("토"),
+    SUNDAY("일");
+
+    companion object {
+        fun convertDayOfWeek(weekText: String): DayOfWeek {
+            when (weekText) {
+                "월" -> return MONDAY
+                "화" -> return TUESDAY
+                "수" -> return WEDNESDAY
+                "목" -> return TUESDAY
+                "금" -> return FRIDAY
+                "토" -> return SATURDAY
+            }
+            return SUNDAY
+        }
+    }
+}
--- src/main/kotlin/oncall/OnCallController.kt
+++ src/main/kotlin/oncall/OnCallController.kt
@@ -1,7 +1,5 @@
 package oncall
 
-import java.time.LocalDate
-
 class OnCallController {
     private val inputView = InputView()
 
@@ -12,22 +10,18 @@ class OnCallController {
 
     }
 
-    private fun getEmergencyWorkDays(): Pair<Month, String> {
+    private fun getEmergencyWorkDays(): Pair<Month, DayOfWeek> {
         val emergencyWorkDaysInput = inputView.readEmergencyWorkDays().split(",")
         require(emergencyWorkDaysInput.size == 2) { "[ERROR] 유효하지 않은 입력 값입니다. 다시 입력해 주세요." }
         val (emergencyMonthInput, startDayOfWeekInput) = emergencyWorkDaysInput
         val monthNumber =
             requireNotNull(emergencyMonthInput.toIntOrNull()) { "[ERROR] 유효하지 않은 입력 값입니다. 다시 입력해 주세요." }
         require(monthNumber in 1..12) { "[ERROR] 유효하지 않은 입력 값입니다. 다시 입력해 주세요." }
-        require(startDayOfWeekInput in WEEK.entries.map { it.text }) { "[ERROR] 유효하지 않은 입력 값입니다. 다시 입력해 주세요." }
-        return Pair(Month.convertMonth(monthNumber), startDayOfWeekInput)
+        require(startDayOfWeekInput in DayOfWeek.entries.map { it.text }) { "[ERROR] 유효하지 않은 입력 값입니다. 다시 입력해 주세요." }
+        return Pair(Month.convertMonth(monthNumber), DayOfWeek.convertDayOfWeek(startDayOfWeekInput))
     }
 
     private fun getWeekdayWorkers() = inputView.readWeekdayWorker().split(",").map { Worker(it, false) }
 
     private fun getHolidayWorkers() = inputView.readHolidayWorker().split(",").map { Worker(it, true) }
-
-    private fun getEmergencyMonth(worker: List<Worker>) {
-
-    }
 }
