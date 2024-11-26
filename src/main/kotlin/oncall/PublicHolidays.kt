03beda474122828af4d31306c99594c6a25c061f
윤동현 <82762769+donghyun81@users.noreply.github.com>
2024-11-26 19:40:23 +0900

    feat(PublicHolidays): 공휴일 열거형 객체 추가

---
+++ src/main/kotlin/oncall/PublicHolidays.kt
@@ -0,0 +1,12 @@
+package oncall
+
+enum class PublicHolidays(val month: Int, val day: Int) {
+    NEW_YEAR_DAY(1, 1),
+    MARCH_FIRST(3, 1),
+    CHILDREN_DAY(5, 5),
+    MEMORIAL_DAY(6, 6),
+    LIBERATION_DAY(8, 15),
+    NATIONAL_FOUNDATION_DAY(10, 3),
+    HANGUL_DAY(10, 9),
+    CHRISTMAS(12, 25)
+}
