e03ca24d963de638b069133ea57bd529951bafbd
윤동현 <82762769+donghyun81@users.noreply.github.com>
2024-12-11 15:14:41 +0900

    feat(DayOfWeek): 요일 정보를 가지는 열거형 객체 추가

---
+++ src/main/kotlin/oncall/DayOfWeek.kt
@@ -0,0 +1,11 @@
+package oncall
+
+enum class DayOfWeek(val useName: String) {
+    MONDAY("월"),
+    TUESDAY("화"),
+    WEDNESDAY("수"),
+    THURSDAY("목"),
+    FRIDAY("금"),
+    SATURDAY("토"),
+    SUNDAY("일")
+}
