256a7aae1284ef7be9725cc23b3047c0c2ff4be3
윤동현 <82762769+donghyun81@users.noreply.github.com>
2024-12-11 16:37:11 +0900

    기능 구현 끝

--- src/main/kotlin/oncall/WorkerService.kt
+++ src/main/kotlin/oncall/WorkerService.kt
@@ -17,6 +17,7 @@ class WorkerService(
             holidayWorkersQueue.add(worker)
             return worker
         }
+         
         val worker = extractWorker(weekDayWorkersQueue)
         weekDayWorkersQueue.add(worker)
         return worker
