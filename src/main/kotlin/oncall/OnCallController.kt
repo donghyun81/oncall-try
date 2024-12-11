b6af4d3fc1cabacef10610524fb4fc8ac5115b4b
윤동현 <82762769+donghyun81@users.noreply.github.com>
2024-12-11 15:52:34 +0900

    feat(OnCallController): 달과 요일로 객체를 변경하는 기능 추가

--- src/main/kotlin/oncall/OnCallController.kt
+++ src/main/kotlin/oncall/OnCallController.kt
@@ -5,8 +5,20 @@ class OnCallController {
     private val inputView = InputView()
 
     fun run() {
-        val (month, startDayOfWeek) = inputView.readMonthAndDayOfWeek()
+        val (monthNumber, dayOfWeekUseName) = inputView.readMonthAndDayOfWeek()
+        val month = convertMonth(monthNumber)
+        val startDayOfWeek = convertDayOfWeek(dayOfWeekUseName)
         val weekDayWorkers = inputView.readWeekDayWorkers()
         val holidayWorkers = inputView.readHolidayWorkers()
+    }
+
+    private fun convertMonth(monthNumber: Int): Month {
+        val month = Month.entries.find { it.number == monthNumber }
+        return requireNotNull(month) { "[ERROR] 유효하지 않은 입력 값입니다. 다시 입력해 주세요." }
+    }
+
+    private fun convertDayOfWeek(dayOfWeekUseName: String): DayOfWeek {
+        val dayOfWeek = DayOfWeek.entries.find { it.useName == dayOfWeekUseName }
+        return requireNotNull(dayOfWeek) { "[ERROR] 유효하지 않은 입력 값입니다. 다시 입력해 주세요." }
     }
 }
