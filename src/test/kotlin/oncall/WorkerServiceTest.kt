37bbf3d2b347a6534d3b38f8bec236f10c4e98b9
윤동현 <82762769+donghyun81@users.noreply.github.com>
2024-12-11 15:46:49 +0900

    test(WorkerService): 근무자 반환 테스트
    - 평일 근무자만 반환하는지 테스트
    - 주말 근무자만 반환하는지 테스트
    - 전부 조회 했을때 다시 추가되었는지 테스트

---
+++ src/test/kotlin/oncall/WorkerServiceTest.kt
@@ -0,0 +1,34 @@
+package oncall
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+
+class WorkerServiceTest {
+
+    private lateinit var workerService: WorkerService
+
+    @BeforeEach
+    fun setUp() {
+        val weekdayWorkers = listOf("준팍", "도밥", "고니", "수아", "루루", "글로")
+        val holidayWorkers = listOf("수아", "글로", "고니", "도밥", "준팍")
+        workerService = WorkerService(weekdayWorkers, holidayWorkers)
+    }
+
+    @Test
+    fun `평일 근무자 반환 테스트`() {
+        val weekdayWorkers = listOf("준팍", "도밥", "고니", "수아", "루루", "글로", "준팍")
+        weekdayWorkers.forEach { worker ->
+            assertEquals(workerService.getWorker(false), worker)
+        }
+    }
+
+    @Test
+    fun `주말 근무자 반환 테스트`() {
+        val weekdayWorkers = listOf("수아", "글로", "고니", "도밥", "준팍", "수아")
+        weekdayWorkers.forEach { worker ->
+            assertEquals(workerService.getWorker(true), worker)
+        }
+    }
+}
